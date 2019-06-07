92891b6cbd927ae7f39bba75b6ce42793def97c2
gongfukangEE <gongfukang@outlook.com>
2019-06-07 10:26:45 +0800

    Initial commit

---
+++ src/main/java/com/daydreamdev/secondskill/SecondsKillApplication.java
@@ -0,0 +1,13 @@
+package com.daydreamdev.secondskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondsKillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondsKillApplication.class, args);
+	}
+
+}
